810f36d77b883b3d7c1cf4112b2848da94154056
Vincent Hoong <vyshoong9509@gmail.com>
2021-09-27 14:51:36 +0800

    Build Static Website

---
+++ dist/js/chunk-071c0f2c.36b519a3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-071c0f2c"],{"082b":function(n,t,e){},"2a36":function(n,t,e){"use strict";e("082b")},"9e4c":function(n,t,e){"use strict";e.r(t);var c=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("Footer")],1)},o=[],u=e("088a"),a={components:{Footer:u["a"]}},r=a,s=(e("2a36"),e("2877")),i=Object(s["a"])(r,c,o,!1,null,"57f9bdbf",null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-071c0f2c.36b519a3.js.map
