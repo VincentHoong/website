943e6046c7d887c69ae6c3eb1d2508072f2d61d3
Vincent Hoong <vyshoong9509@gmail.com>
2021-10-11 15:17:07 +0800

    Add Facebook Chat Plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import fbChat from './plugins/fbChat'
 import "./assets/css/global.css"
 import './mixins/main'
 import store from './store'
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  fbChat,
   store,
   render: h => h(App)
 }).$mount('#app')
