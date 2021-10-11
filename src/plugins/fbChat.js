3545d28cb6a3690cccff88ed714ebfe45827fbb3
Vincent Hoong <vyshoong9509@gmail.com>
2021-10-11 15:19:35 +0800

    Change Facebook Chat Plugin Theme

--- src/plugins/fbChat.js
+++ src/plugins/fbChat.js
@@ -3,6 +3,6 @@ import VueFbCustomerChat from 'vue-fb-customer-chat'
 
 Vue.use(VueFbCustomerChat, {
   page_id: '113376811116607', //  change 'null' to your Facebook Page ID,
-  theme_color: '#333333', // theme color in HEX
+  theme_color: '#004544', // theme color in HEX
   locale: 'en_US', // default 'en_US'
 })
